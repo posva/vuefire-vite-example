abca30847b5b5fde25885139a5e826b6183e5969
Eduardo San Martin Morote <posva13@gmail.com>
2022-12-19 15:19:00 +0100

    add analytics

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import App from './App.vue'
 import { VueFire } from 'vuefire'
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
+import { getAnalytics } from 'firebase/analytics'
 
 // Initialize Firebase
 const firebaseApp = initializeApp({
@@ -14,6 +15,8 @@ const firebaseApp = initializeApp({
   projectId: 'vitesse-vuefire-example',
   appId: '1:682870035896:web:8bac46c2357e0bd147eb4d',
 })
+
+getAnalytics(firebaseApp)
 
 createApp(App)
   // install VueFire and provide the firebase app to components
